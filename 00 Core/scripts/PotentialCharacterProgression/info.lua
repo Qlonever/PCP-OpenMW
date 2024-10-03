84f876d15d09525336b302052a35f596f3489195
Qlonever <42286723+Qlonever@users.noreply.github.com>
2024-10-03 16:20:31 -0400

    logic fixes, detect settings version

--- 00 Core/scripts/PotentialCharacterProgression/info.lua
+++ 00 Core/scripts/PotentialCharacterProgression/info.lua
@@ -7,5 +7,6 @@ return {
     name = 'PotentialCharacterProgression',
     version = '1.0.1',
     saveVersion = 1,
+    settingsVersion = 1,
     minApiVersion = 68
 }
--- 00 Core/scripts/PotentialCharacterProgression/player.lua
+++ 00 Core/scripts/PotentialCharacterProgression/player.lua
@@ -46,14 +46,19 @@ local balanceSettings = storage.playerSection('SettingsPlayer' .. info.name .. '
 local skillUpsPerLevel = core.getGMST('iLevelupTotal')
 local levelHealthMult = core.getGMST('fLevelUpHealthEndMult')
 
--- Data
+-- Player data
 
-local playerClassRecord = Player.classes.record(Player.record(self).class)
-local playerRaceRecord = Player.races.record(Player.record(self).race)
-local playerSex
 local playerStats = Player.stats
 local playerAttributes = playerStats.attributes
 local playerSkills = playerStats.skills
+
+local function getPlayerRecords()
+    return {
+        class = Player.classes.record(Player.record(self).class),
+        race = Player.races.record(Player.record(self).race),
+        sex = (Player.record(self).isMale and 'male') or 'female'
+    }
+end
 
 -- Saved variables
 
@@ -108,6 +113,7 @@ local levelUpData
 
 
 
+
 -- Debug stuff -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 local function infoDump()
@@ -124,6 +130,38 @@ local function infoDump()
     print('total skill increases this level: ' .. totalSkillUpsCurLevel)
     print('level-ups: ' .. levelUps)
     print('experience: ' .. experience)
+end
+
+
+
+
+
+
+
+-- Health functions  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+-- Increase max health, apply increase to current health as well
+local function increaseHealth(healthIncrease)
+    totalHealthGained = totalHealthGained + healthIncrease
+    playerStats.dynamic.health(self).base = playerStats.dynamic.health(self).base + healthIncrease
+    playerStats.dynamic.health(self).current = math.max(playerStats.dynamic.health(self).current + healthIncrease, 1)
+end
+
+-- Recalculate retroactive starting health
+local function calculateRetroStartHealth()
+    local playerRecords = getPlayerRecords()
+    local currentEndurance = playerAttributes.endurance(self).base
+    local currentStrength = playerAttributes.strength(self).base
+    local initAttributes = {
+        strength = playerRecords.race.attributes.strength[playerRecords.sex],
+        endurance = playerRecords.race.attributes.endurance[playerRecords.sex]
+    }
+    for _, attributeid in pairs(playerRecords.class.attributes) do
+        if attributeid == 'strength' or attributeid == 'endurance' then
+            initAttributes[attributeid] = initAttributes[attributeid] + 10
+        end
+    end
+    return (currentStrength + currentEndurance - initAttributes.strength - initAttributes.endurance) * 0.5
 end
 
 
@@ -216,24 +254,12 @@ local function hideMenu()
         if playerSettings:get('RetroactiveHealth') then
             healthIncrease = healthIncrease * levelUps - totalHealthGained
             if playerSettings:get('RetroactiveStartHealth') then
-                local currentStrength = Player.stats.attributes.strength(self).base
-                local initAttributes = {
-                    strength = playerRaceRecord.attributes.strength[playerSex],
-                    endurance = playerRaceRecord.attributes.endurance[playerSex]
-                }
-                for _, attributeid in pairs(playerClassRecord.attributes) do
-                    if attributeid == 'strength' or attributeid == 'endurance' then
-                        initAttributes[attributeid] = initAttributes[attributeid] + 10
-                    end
-                end
-                healthIncrease = healthIncrease + (currentStrength + currentEndurance - initAttributes.strength - initAttributes.endurance) * 0.5
+                healthIncrease = healthIncrease + calculateRetroStartHealth()
             end
         else
             healthIncrease = healthIncrease * levelUpData.ups
         end
-        totalHealthGained = totalHealthGained + healthIncrease
-        Player.stats.dynamic.health(self).base = Player.stats.dynamic.health(self).base + healthIncrease
-        Player.stats.dynamic.health(self).current = math.max(Player.stats.dynamic.health(self).current + healthIncrease, 1)
+        increaseHealth(healthIncrease)
     end
     isLevelUp = true
     levelUpData = nil
@@ -261,9 +287,7 @@ local function finishMenu(data)
                 healthIncrease = healthIncrease + (data.uiAttributes.endurance.ups + data.uiAttributes.strength.ups) * 0.5
             end
         end
-        totalHealthGained = totalHealthGained + healthIncrease
-        Player.stats.dynamic.health(self).base = Player.stats.dynamic.health(self).base + healthIncrease
-        Player.stats.dynamic.health(self).current = Player.stats.dynamic.health(self).current + healthIncrease
+        increaseHealth(healthIncrease)
     end
     
     experience = data.uiExperience
@@ -291,12 +315,13 @@ local function handleskillUps(skillid, source, options)
     skillData[skillid].upsCurLevel = skillData[skillid].upsCurLevel + skillIncrease
     totalSkillUpsCurLevel = totalSkillUpsCurLevel + skillIncrease
     options.levelUpProgress = skillIncrease
-    
-    -- Increase attribute potential depending on skill major/minor/misc status
+
+    local playerRecords = getPlayerRecords()
+
     local potentialMult = balanceSettings:get('PotentialPerSkill')
-    if contains(playerClassRecord.minorSkills, skillid) then
+    if contains(playerRecords.class.minorSkills, skillid) then
         potentialMult = balanceSettings:get('PotentialPerMinorSkill')
-    elseif contains(playerClassRecord.majorSkills, skillid) then
+    elseif contains(playerRecords.class.majorSkills, skillid) then
         potentialMult = balanceSettings:get('PotentialPerMajorSkill')
     end
     attributeData[options.levelUpAttribute].potential = attributeData[options.levelUpAttribute].potential + skillIncrease * potentialMult
@@ -317,11 +342,20 @@ I.SkillProgression.addSkillLevelUpHandler(handleskillUps)
 
 -- Record skill values when finishing character creation or when first loading this script on an existing character
 local function finishCharGen()
+    -- Update health with relevant settings
+    -- Have to check this because you can, in fact, level up before finishing character creation
+    if levelUps == 0 then
+        -- Birthsign bonuses should affect starting health immediately
+        if playerSettings:get('RetroactiveHealth') and playerSettings:get('RetroactiveStartHealth') then
+            local healthIncrease = calculateRetroStartHealth()
+            increaseHealth(healthIncrease)
+        end
+    end
+
     for skillid, skill in pairs(playerSkills) do
         skillData[skillid].peak = skill(self).base
     end
-    playerSex = (Player.record(self).isMale and 'male') or 'female'
-    totalSkillUpsCurLevel = Player.stats.level(self).progress % skillUpsPerLevel
+    totalSkillUpsCurLevel = playerStats.level(self).progress % skillUpsPerLevel
     local keybind = input.getKeyName(playerSettings:get('MenuKey'))
     local charGenCallback = async:registerTimerCallback('charGenMessage', function() ui.showMessage(L('StartUp', {keybind = keybind}), {showInDialogue = false}) end)
     async:newSimulationTimer(0.1, charGenCallback)
@@ -362,12 +396,19 @@ end
 
 local function onLoad(data)
     if info.saveVersion > data.saveVersion then
-        ui.showMessage(L('VersionOld'), {showInDialogue = false})
-        print(L('VersionOld'))
+        ui.showMessage(L('SaveVersionNew'), {showInDialogue = false})
+        print(L('SaveVersionNew'))
     elseif info.saveVersion < data.saveVersion then
-        ui.showMessage(L('VersionNew'), {showInDialogue = false})
-        print(L('VersionNew'))
+        ui.showMessage(L('SaveVersionOld'), {showInDialogue = false})
+        print(L('SaveVersionOld'))
     else
+        if info.settingsVersion > (data.settingsVersion or 1) then
+            ui.showMessage(L('SettingsVersionNew'), {showInDialogue = false})
+            print(L('SettingsVersionNew'))
+        elseif info.settingsVersion < (data.settingsVersion or 1) then
+            ui.showMessage(L('SettingsVersionNew'), {showInDialogue = false})
+            print(L('SettingsVersionOld'))
+        end
         skillData = data.skillData
         attributeData = data.attributeData
         levelUps = data.levelUps
@@ -375,13 +416,13 @@ local function onLoad(data)
         totalHealthGained = data.totalHealthGained
         totalSkillUpsCurLevel = data.totalSkillUpsCurLevel
     end
-        playerSex = (Player.record(self).isMale and 'male') or 'female'
         isCharGenFinished = true
 end
 
 local function onSave()
     return {
         saveVersion = info.saveVersion,
+        settingsVersion = info.settingsVersion,
         skillData = skillData,
         attributeData = attributeData,
         levelUps = levelUps,
