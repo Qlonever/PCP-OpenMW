8ce554330252764ad12cd7cb7895f29d7e81783d
Qlonever <42286723+Qlonever@users.noreply.github.com>
2025-07-28 04:36:18 -0400

    minor interface fix

--- 00 Core/scripts/PotentialCharacterProgression/p_interface.lua
+++ 00 Core/scripts/PotentialCharacterProgression/p_interface.lua
@@ -3,15 +3,15 @@
 -- Variables included in interface:
     -- KEY ...................... DESCRIPTION
     
-    -- levelUps ................. The number of levels the player has gained with this mod, generally one less than player level unless installed mid-playthrough
+    -- levelUps ................. The number of levels the player has gained with this mod, used by the "Retroactive Health Gain" setting
     -- experience ............... The number of experience points the player currently has to spend in the potential menu
     -- totalHealthGained ........ The total amount this mod has added to the player's max health, does not include health granted by vanilla character creation or from other sources
     -- totalSkillUpsCurLevel .... The current number of skill increases contributing towards gaining another level-up, does not include skill increases contributing to queued level-ups
     -- attributeData ............ Data tables containing variables for each attribute, accessible by attribute record IDs
-        -- ups ...................... The total value the attribute has been increased by this mod
-        -- potential ................ The potential of the attribute, stored as a number of points over the attribute's base value rather than the actual limit visible in the menu
+        -- ups ...................... The total points the attribute has gained from this mod, used by the "Realistic Retroactive Health Gain" setting
+        -- potential ................ The potential of the attribute, stored as points over the attribute's base value rather than the limit visible in the menu
     -- skillData ................ Data tables containing variables for each skill, accessible by skill record IDs
-        -- ups ...................... The total value the skill has been increased by while this mod was installed, currently unused but still tracked
+        -- ups ...................... The total points the skill has been increased by while this mod was installed, currently unused but still tracked
         -- upsCurLevel .............. The number of increases contributed by the skill towards gaining another level-up, used to select level-up class art
         -- upsLastLevels ............ The number of increases contributed by the skill towards queued level-ups, used to select level-up class art
         -- peak ..................... The highest value reached by the skill while this mod was installed, used by the "Allow Jail Time Exploit" setting
@@ -20,19 +20,21 @@
     -- FUNCTION(args) ........................... DESCRIPTION
     
     -- get(key) ................................. Returns the variable with the given key
-    -- set(key, value) .......................... Sets the variable with the given key equal to the given value
-    -- inc(key, value) .......................... Adds the value to the variable with the given key, returns the incremented value
+    -- set(key, value) .......................... Sets the variable with the given key equal to the value
+    -- inc(key, value) .......................... Increases the variable with the given key by the value, returns the incremented value
     -- attributeGet(attributeId, key) ........... Returns the attribute's variable with the given key
     -- attributeSet(attributeId, key, value) .... Sets the attribute's variable with the given key equal to the value
-    -- attributeInc(attributeId, key, value) .... Adds the value to the attribute's variable with the given key, returns the incremented value
+    -- attributeInc(attributeId, key, value) .... Increases the attribute's variable with the given key by the value, returns the incremented value
     -- attributeSetAll(key, value) .............. Sets the variable with the given key equal to the value for every attribute
-    -- attributeIncAll(key, value) .............. Adds the value to the variable with the given key for every attribute
+    -- attributeIncAll(key, value) .............. Increases the variable with the given key by the value for every attribute
     -- skillGet(skillId, key) ................... Returns the skill's variable with the given key
     -- skillSet(skillId, key, value) ............ Sets the skill's variable with the given key equal to the value
-    -- skillInc(skillId, key, value) ............ Adds the value to the skill's variable with the given key, returns the incremented value
+    -- skillInc(skillId, key, value) ............ Increases the skill's variable with the given key by the value, returns the incremented value
     -- skillSetAll(key, value) .................. Sets the variable with the given key equal to the value for every skill
-    -- skillIncAll(key, value) .................. Adds the value to the variable with the given key for every skill
+    -- skillIncAll(key, value) .................. Increases the variable with the given key by the value for every skill
     -- reset() .................................. Resets every variable to its initial value
+    
+    -- Note: Inc functions can also accept another key in place of a value, which will increase the first variable by the second
     
 local core = require('openmw.core')
 
@@ -181,7 +183,7 @@ return {
     attributeSet = attributeSet,
     attributeInc = attributeInc,
     attributeSetAll = attributeSetAll,
-    attributeIncAll = attributeSetAll,
+    attributeIncAll = attributeIncAll,
     skillGet = skillGet,
     skillSet = skillSet,
     skillInc = skillInc,
